10810d2d12f2112cff0fca444945ed3589787a07
Douglas A. Ruby <dar@digitaleconomist.org>
2017-07-14 09:56:15 -0500

    Add files via upload

---
+++ ch_title.js
@@ -0,0 +1,39 @@
+//
+// 11-06-2015
+//
+var chx = [
+'ch_01','ch_02','ch_03','ch_04','ch_05',
+'ch_06','ch_07','ch_08','ch_09','ch_10',
+'ch_11','ch_12','ch_13','ch_14','ch_15',
+'ch_16','ch_17','ch_18','ch_19','ch_20',
+'ch_21','ch_22','ch_23','ch_24','ch_25',
+'ch_26','ch_27','ch_28'];
+//
+// was 28
+var ch_total = 13;
+var ch_count = 13;
+//
+var ch_title = [
+"0. An Overview",
+"1. Macroeconomic Activity",
+"2. Macroeconomic Measures",
+"3. Living Standards",
+"4. The Ability to Spend",
+"5 Aggregate Consumption",
+"6. Investment Decisions",
+"7. Interest Rate Determination",
+"8. Financial Markets",
+"9. The Ability to Produce",
+"10. Economic Policy",
+"11. Individual Decision Making",
+"A. Appendix",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+""];
---
+++ objects.js
@@ -0,0 +1,101 @@
+//
+// 11-06-2015
+//
+var lo_total = 47;
+
+var lo_count = [4,4,6,3,2,4,3,3,5,5,2,4,2];
+var title_1 = [
+
+"0.1: What is Economics",
+
+"0.2: Scarcity and Rationing Systems",
+
+"0.3: Microeconomics Markets and Relative Prices",
+
+"0.4 Markets and Equilibrium",
+
+"1.1: Economic Growth",
+
+"1.2: Employment of Resources",
+
+"1.3: Price Stability",
+
+"1.4: International Linkages",
+
+"2.1: Living Standards and Aggregation",
+
+"2.2: Real GDP and Busines Cycles",
+
+"2.3: Measures of Unemployment",
+
+"2.4: Inflation and Inflationary Expectations",
+
+"2.5: The Balance of Payments",
+
+"2.6: Exchange Rates ",
+
+"3.1: Living Standards",
+
+"3.2: Microfoundations: Production and Output",
+
+"3.3: Capital Accumulation, Living Standards and Growth",
+
+"4.1: Aggregate Demand",
+
+"4.2: Aggregate Expenditure and Income Determination",
+
+
+"5.1: Microfoundations: Individual Consumption and Savings Decisions",
+"5.2: Aggregate Consumption Behavior",
+
+"5.3: The Permanent Income Hypothesis",
+"5.3: The Life Cycle Hypothesis",
+
+"6.1: Microfoundations: Investment Decisions and Profit Maximization",
+
+"6.2: Investment: The Accelerator",
+
+"6.3: Tobin's-Q ",
+
+"7.1: Real Interest Rates",
+
+"7.2: Savings and Investment",
+
+"7.3: The Term and Risk Structure of Interest Rates ",
+
+"8.1: Asset Prices and Yields",
+
+
+"8.2: Money as a Financial Asset",
+
+"8.3: Money Demand: The Inventory Model",
+
+"8.4: Money Demand: Portfolio Analysis",
+
+"8.5: Central Banking and Monetary Policy",
+
+"9.1: Microfoundations: Labor Supply Decisions",
+
+"9.2: Price Level Determination",
+
+"9.3: Price Expectations and Aggregate Supply",
+
+"9.4: The Quantity Equation",
+
+"9.5: Rational Expectations",
+
+"10.1: Demand-Side Policy",
+
+"10.2: Supply Side Policies ",
+
+"11.1: Utility and Preferences",
+
+"11.2: The Budget Constraint",
+"11.3: Indifference Curve Analysis",
+
+"11.4: A Consumer Optimum "
+,
+"A.1. Equations",
+"A.2. Recommended Readings",
+"","","","","","","","","","",""
+];
